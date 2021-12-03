fe2d8d2b7e77609317892df42d4f848b1472a9c5
Jaeyong Sung <jaeyong0201@gmail.com>
2021-12-03 13:53:32 +0900

    solved day1 part2

--- lib/aoc2021/main.ex
+++ lib/aoc2021/main.ex
@@ -3,7 +3,8 @@ defmodule Aoc2021.Main do
 
   def main(args) do
     case args do
-      ["day1"] -> Aoc2021.Day1.main()
+      ["day1", "part1"] -> Aoc2021.Day1.part1()
+      ["day1", "part2"] -> Aoc2021.Day1.part2()
       [] -> IO.puts("Oops")
     end
   end
