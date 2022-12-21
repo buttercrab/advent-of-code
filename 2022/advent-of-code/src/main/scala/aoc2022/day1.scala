0897c467f991856804b969d618ea5416c5e631c5
Jaeyong Sung <jaeyong0201@gmail.com>
2022-12-22 03:27:55 +0900

    fix day 1

--- 2022/advent-of-code/src/main/scala/aoc2022/day1.scala
+++ 2022/advent-of-code/src/main/scala/aoc2022/day1.scala
@@ -3,12 +3,12 @@ package aoc2022
 import scala.io.Source
 
 def day1(part: Int): Unit =
-  var s: Int = 0
-  var v: Array[Int] = new Array(0)
-  for line <- Source.fromFile("input/1.txt").getLines() do
-    if line.isEmpty then
-      v = v :+ s
-      s = 0
-    else s += line.toInt
+  val v = Source
+    .fromFile("input/1.txt")
+    .getLines()
+    .foldLeft(Array.empty[Int] :+ 0)((arr, s) =>
+      if s.isEmpty then arr :+ 0
+      else arr.init :+ (arr.last + s.toInt)
+    )
   if part == 1 then println(v.max)
   else println(v.sorted.reverse.slice(0, 3).sum)
